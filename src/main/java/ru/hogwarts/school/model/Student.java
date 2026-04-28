f79a3e3a70ea92af9b3b234514a3422dc3266e9f
Бексултан <nightwing_14@Mail.ru>
2026-04-28 06:46:18 +0300

    Start

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -3,13 +3,11 @@ package ru.hogwarts.school.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import jakarta.persistence.Id;
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Setter @Getter
 @Entity
 public class Student {
